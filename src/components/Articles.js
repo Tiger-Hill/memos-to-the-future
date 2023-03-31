b6a5e8a0bd73d94a54d72d1161e124a223dc8a84
aminmallol <aminm-34@hotmail.fr>
2023-03-31 01:09:37 +0100

    Added the selected clients section

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,10 +1,17 @@
 import { articlesData } from "../articlesData";
 import ArticleCard from "./ArticleCard";
 
+import selectedClientsImg from "../images/selected-clients.jpeg";
+
 const Articles = () => {
   return (
     <div className="article-container">
       {articlesData.map(article => <ArticleCard articleData={article}/>)}
+
+      <section className="selected-clients">
+        <h3>Selected clients:</h3>
+        <img src={selectedClientsImg} alt="Logos of the selected clients" />
+      </section>
     </div>
   );
 }
